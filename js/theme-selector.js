8ccc22c468d018f05b0587086410d7ec208bee90
RicciCev <ricardo_christmann_apps1oa1920@cev.com>
2021-06-03 11:05:46 +0200

    Pequeños cambios relacionados a la nomenclatura de variables y texto cambiado a modo oscuro

--- js/theme-selector.js
+++ js/theme-selector.js
@@ -1,7 +1,7 @@
-/* Referencia al botón switch a través de su id. */
-let darkTheme = document.getElementById('theme-toggler');
+/* Referencia al botón switch a través de su id, y a los iconos. */
+let themeToggler = document.getElementById('theme-toggler');
 
 /* Cambiar el tema de color a oscuro mediante una función que activa y desactiva la clase 'dark' en el documnento html. */
-darkTheme.addEventListener('click', () => {
-    document.body.classList.toggle('dark')
+themeToggler.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
 })
